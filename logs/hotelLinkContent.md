a1fc24315208eb12121318b333d34cc0dc9532ea
森川弘世 <morizou0718@gmail.com>
2026-08-02 06:11:32 +0900

    feat: featured_stay全国展開（全国トリアージ一括適用・おすすめ宿カード簡素化）

--- logs/hotelLinkContent.md
+++
@@ -1,6 +0,0 @@
-# Hotel Link Content Check
-total: 1380  NG_rakuten: 0  NG_jalan: 0
-
-## NG rakuten
-
-## NG jalan
--- scratch/_check.js
+++
@@ -1,6 +0,0 @@
-const d=require('./src/data/destinations.json');
-const withCoords = d.filter(x=>x.lat && x.lng);
-const without = d.filter(x=>!(x.lat && x.lng));
-console.log('with coords:', withCoords.length, 'without:', without.length);
-console.log('IDs without:');
-without.forEach(x=>console.log(x.id, x.name));
